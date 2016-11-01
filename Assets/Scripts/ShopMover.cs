ad5d662b6d1f813bbcb85617fb9fb07cf1b8be64
timoffex <timoffex@gmail.com>
2016-10-31 23:09:41 -0700

    moved scripts to Scripts folder

---
+++ Assets/Scripts/ShopMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Defines methods for moving an object between shop locations.
+/// </summary>
+public abstract class ShopMover : MonoBehaviour {
+
+	/// <summary>
+	/// Returns position inside the shop.
+	/// </summary>
+	/// <returns>The position.</returns>
+	public abstract IntPair GetPosition ();
+
+
+
+	public delegate void SuccessCallback (bool success);
+
+	/// <summary>
+	/// Moves to the specified position and calls the callback with true on success or false on failure.
+	/// </summary>
+	/// <param name="pos">Position.</param>
+	/// <param name="callback">Callback that takes a boolean which is "true" on success and "false" otherwise.</param>
+	public abstract IEnumerator MoveToPosition (IntPair pos, SuccessCallback callback = null);
+}
