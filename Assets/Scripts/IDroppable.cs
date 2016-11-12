50e9e3ff950bf31d680ba62818bf09660dd48ed8
timoffex <timoffex@gmail.com>
2016-11-11 20:38:46 -0800

    LootDropper and IDroppable
    LootDropper should be placed on objects that will drop items. Scripts
    on those objects will call the LootDropper’s DropItem () function.
    
    IDroppable is an optional interface that any droppable object can
    implement in some script. IDroppable’s DropAt(shop, location) is called
    when the item is about to be dropped by LootDropper’s DropItem ().

---
+++ Assets/Scripts/IDroppable.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IDroppable {
+	void DropAt (Shop shop, IntPair location);
+}
---
+++ Assets/Scripts/LootDropper.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LootDropper : MonoBehaviour {
+
+	public Transform droppedPrefab;
+
+	/// <summary>
+	/// Helper function to drop the given droppedPrefab.
+	/// </summary>
+	public virtual void DropItem () {
+		IDroppable dropIface = droppedPrefab.GetComponent<IDroppable> ();
+
+
+		if (dropIface) {
+			// If droppedPrefab has component IDroppable, use its DropAt () function.
+			dropIface.DropAt (GetShop (), GetLocation ());
+		} else {
+			// Otherwise, just use a default dropping animation.
+
+			var clone = GameObject.Instantiate (droppedPrefab, transform.parent) as GameObject;
+
+			clone.transform.position = transform.position;
+
+		}
+	}
+
+
+	private Shop myShop;
+	private Shop GetShop () {
+		if (!myShop)
+			myShop = GameObject.Find ("Room").GetComponent<Shop> (); // TODO SHOP
+
+		return myShop;
+	}
+
+	private IntPair GetLocation () {
+		return GetShop ().worldToShopCoordinates (transform.position);
+	}
+}
