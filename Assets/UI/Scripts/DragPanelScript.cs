ce4401767cd10d1e79b7bf2bc6e962a9f685dac1
timoffex <timoffex@gmail.com>
2016-11-29 19:52:15 -0800

    inventory dragging

---
+++ Assets/UI/Scripts/DragPanelScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class DragPanelScript : MonoBehaviour, IPointerDownHandler, IDragHandler {
+
+	private RectTransform panelRect;
+	private Vector2 localOffset;
+
+	// Use this for initialization
+	void Awake () {
+		panelRect = transform.parent.gameObject.GetComponentInParent<RectTransform> ();
+	}
+
+	public void OnPointerDown (PointerEventData data) {
+		panelRect.SetAsLastSibling ();
+
+		localOffset = (Vector2)panelRect.position - data.position;
+	}
+
+	public void OnDrag (PointerEventData data) {
+		panelRect.position = data.position + localOffset;
+	}
+}
