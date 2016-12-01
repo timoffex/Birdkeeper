4ba49748bb5bfdc385e4231df6f243197e42c0c4
timoffex <timoffex@gmail.com>
2016-12-01 13:52:53 -0800

    another bug fix in SerializableDictionary

--- Assets/Scripts/Utility/SerializeableDictionary.cs
+++ Assets/Scripts/Utility/SerializeableDictionary.cs
@@ -7,10 +7,10 @@ using System.Collections.Generic;
 [Serializable]
 public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver {
 	[SerializeField]
-	private List<TKey> keys = new List<TKey>();
+	private KeyList keys = new KeyList ();
 
 	[SerializeField]
-	private List<TValue> values = new List<TValue>();
+	private ValueList values = new ValueList ();
 
 	// save the dictionary to lists
 	public void OnBeforeSerialize()
@@ -31,14 +31,18 @@ public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, IS
 	}
 
 	// load dictionary from lists
-	public void OnAfterDeserialize()
-	{
+	public void OnAfterDeserialize() {
 		this.Clear();
 
-		if(keys.Count != values.Count)
-			throw new System.Exception(string.Format("there are {0} keys and {1} values after deserialization. Make sure that both key and value types are serializable."));
-
-		for(int i = 0; i < keys.Count; i++)
-			this.Add(keys[i], values[i]);
+		if (keys.Count != values.Count)
+			throw new System.Exception (string.Format ("there are {0} keys and {1} values after deserialization. Make sure that both key and value types are serializable."));
+		else {
+			for (int i = 0; i < keys.Count; i++)
+				this.Add (keys [i], values [i]);
+		}
 	}
+
+
+	[Serializable] private class KeyList : List<TKey> { }
+	[Serializable] private class ValueList : List<TValue> { }
 }
