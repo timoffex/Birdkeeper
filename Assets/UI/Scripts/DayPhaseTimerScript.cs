1af0e2fa466ceb276bf23ecb66c49abaa3df3e1d
timoffex <timoffex@gmail.com>
2016-12-02 00:13:08 -0800

    day/edit phase cycle

---
+++ Assets/UI/Scripts/DayPhaseTimerScript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayPhaseTimerScript : MonoBehaviour {
+
+	public GamePhase nextPhase;
+	public float maximumTime = 60;
+	private float startTime;
+
+
+	public Text timePassedText;
+
+	void Start () {
+		startTime = Time.time;
+	}
+
+	void Update () {
+		float timeDelta = Time.time - startTime;
+
+		if (timeDelta >= maximumTime)
+			Game.current.SwitchToPhase (nextPhase);
+
+		if (timePassedText != null)
+			timePassedText.text = string.Format ("{0} / {1}", (int)timeDelta, maximumTime);
+	}
+}
+
+
