1ebcd3c207fce2308f48c7c21ea99614ab10f9ef
timoffex <timoffex@gmail.com>
2016-12-07 13:08:36 -0800

    longer day phase + longer spawn delay

--- Assets/UI/Scripts/Customers/CustomerSpawnScheduler.cs
+++ Assets/UI/Scripts/Customers/CustomerSpawnScheduler.cs
@@ -7,6 +7,7 @@ public class CustomerSpawnScheduler : MonoBehaviour {
 	public CustomerSpawner customerSpawner;
 	public ShopMover customerPrefab;
 
+
 	void Start () {
 		StartCoroutine (SpawningRoutine ());
 	}
@@ -16,7 +17,7 @@ public class CustomerSpawnScheduler : MonoBehaviour {
 		while (true) {
 			ShopMover customer = GameObject.Instantiate (customerPrefab) as ShopMover;
 			customerSpawner.SpawnCustomer (customer);
-			yield return new WaitForSeconds (3 + Random.value * 2);
+			yield return new WaitForSeconds (10 + Random.value * 10);
 		}
 	}
 }
